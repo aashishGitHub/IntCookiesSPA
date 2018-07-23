8cb1a27b4eeb9d54a8a3104c6c0a84f847f98d62
Aashish Kumar <aashish123pqr@gmail.com>
2018-07-23 08:54:15 +0530

    db Backup

---
+++ IntCookiesDbBackup.sql
@@ -0,0 +1,103 @@
+USE [master]
+GO
+
+/****** Object:  Database [InternationalCookies]    Script Date: 07/23/2018 08:46:57 ******/
+CREATE DATABASE [InternationalCookies] ON  PRIMARY 
+( NAME = N'FunDb', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL10_50.DEV2008\MSSQL\DATA\FunDb.mdf' , SIZE = 3072KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'FunDb_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL10_50.DEV2008\MSSQL\DATA\FunDb_log.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+GO
+
+ALTER DATABASE [InternationalCookies] SET COMPATIBILITY_LEVEL = 100
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [InternationalCookies].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [InternationalCookies] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [InternationalCookies] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [InternationalCookies] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [InternationalCookies] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [InternationalCookies] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [InternationalCookies] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [InternationalCookies] SET AUTO_CREATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [InternationalCookies] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [InternationalCookies] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [InternationalCookies] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [InternationalCookies] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [InternationalCookies] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [InternationalCookies] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [InternationalCookies] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [InternationalCookies] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [InternationalCookies] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [InternationalCookies] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [InternationalCookies] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [InternationalCookies] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [InternationalCookies] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [InternationalCookies] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [InternationalCookies] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [InternationalCookies] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [InternationalCookies] SET  READ_WRITE 
+GO
+
+ALTER DATABASE [InternationalCookies] SET RECOVERY FULL 
+GO
+
+ALTER DATABASE [InternationalCookies] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [InternationalCookies] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [InternationalCookies] SET DB_CHAINING OFF 
+GO
+
